325034dbd54784fd0631ef5b2997039d858d199f
bamontelucas <bamontelucas@gmail.com>
2017-05-23 12:00:12 -0300

    version 1.0.0.1

---
+++ image-clipboard/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace image_clipboard
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                System.Console.WriteLine("Informe a imagem por parâmetro!");
+                System.Console.ReadKey(true);
+            } else
+            {
+                System.Drawing.Image img = System.Drawing.Image.FromFile(args[0]);
+                System.Windows.Forms.Clipboard.SetImage(img);
+            }
+        }
+    }
+}
